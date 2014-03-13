f5f0096404ca31c5e7e95b88e427035f8ed5c974
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-13 23:27:42 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- helper/dinner_functions.sh
+++ helper/dinner_functions.sh
@@ -88,7 +88,7 @@ function _add_device_config () {
 		head -5  ${DINNER_CONF_DIR}/example.dist
 		printf "DINNER CONFIG FILE (Source: https://github.com/NemesisRE/dinner) developed by NemesisRE (https://nrecom.net)" > ${DINNER_CONF_DIR}/${DEVICE_CONFIG_NAME}
 		for VARIABLE in $(cat ${DINNER_CONF_DIR}/example.dist | sed 's/^#//g' | sed '/^#/ d' | awk -F= '{ print $1 }' ); do
-			_e "${BLDWHT} "DESCRIPTION" "${VARIABLE_DESC:-\"No Description available\"} (Dinnerdefault: ${!VARIABLE:-none})"
+			_e "${BLDWHT}" "DESCRIPTION" "${VARIABLE_DESC:-\"No Description available\"} (Dinnerdefault: ${!VARIABLE:-none})"
 			_e_pending " "  "${VARIABLE}" "${BLDWHT}" "0"
 			read USERVALUE
 			[[ ${USERVALUE} ]] && eval "${VARIABLE}=${USERVALUE}" >> ${DINNER_CONF_DIR}/${DEVICE_CONFIG_NAME}
