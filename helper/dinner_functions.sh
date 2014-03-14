a1be1e023d602aa88a57fc995543824efa8f8e53
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-14 02:28:20 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- helper/dinner_functions.sh
+++ helper/dinner_functions.sh
@@ -119,9 +119,9 @@ function _check_prerequisites () {
 	printf "${DINNER_LOG_COMMENT}\nThis Error Log contains only messages from STDERR\n\n" &> ${CURRENT_ERRLOG:-${DINNER_LOG_DIR}/dinner_error.log}
 
 	if [ -f "${DINNER_DIR}/config.d/${CURRENT_CONFIG}" ]; then
-		if [ "$(sed -n '1{p;q;}' ${DEVICE_CONFIG_NAME})" != "${DINNER_CONFIG_HEADER}" ] && ; then
+		if [ "$(sed -n '1{p;q;}' ${DEVICE_CONFIG_NAME})" != "${DINNER_CONFIG_HEADER}" ] ; then
 			_e_fatal "${CURRENT_CONFIG} is not a valid dinner config."
-		elif [[ "$(sed -n '2{p;q;}' ${DEVICE_CONFIG_NAME})" != "${DINNER_CONFIG_VERSION}" ]]; then
+		elif [ "$(sed -n '2{p;q;}' ${DEVICE_CONFIG_NAME})" != "${DINNER_CONFIG_VERSION}" ]; then
 			_e_warn "Config version differs from current version" "Version of ${DEVICE_CONFIG_NAME}: $(sed -n '2{p;q;}' ${DEVICE_CONFIG_NAME} | awk -F_ '{print $3}')" "Current version: $(echo ${DINNER_CONFIG_VERSION} | awk -F_ '{print $3}')"
 		fi
 		_exec_command "source ${DINNER_DIR}/config.d/${CURRENT_CONFIG}"
