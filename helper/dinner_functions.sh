93053cee4433f746587c9b094ece8d1a09f685ae
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-14 01:11:16 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- helper/dinner_functions.sh
+++ helper/dinner_functions.sh
@@ -103,8 +103,8 @@ function _add_device_config () {
 		done
 		IFS=$old_IFS
 		cat ${DINNER_CONF_DIR}/example.dist | sed -e "1,/${VARIABLE}/d" >> ${DINNER_CONF_DIR}/${DEVICE_CONFIG_NAME}
-		_e_success "Here is your new config (${DEVICE_CONFIG_NAME}):"
-		cat ${DINNER_CONF_DIR}/${DEVICE_CONFIG_NAME}
+		_e_success "Succesfully created config \"${DEVICE_CONFIG_NAME}\"
+		printf "${BLDWHT}%s${TXTDEF}\n" "Available Configs:" && _print_configs "\t\t%s\n"
 		exit 0
 	fi
 }
