f4c90b0297d0911c38ae25a0e77b093d8ae83fff
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-14 01:13:58 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- helper/dinner_functions.sh
+++ helper/dinner_functions.sh
@@ -81,10 +81,11 @@ function _add_device_config () {
 				_e_pending_skipped "Will not overwrite existing config"
 				exit 0
 			fi
-			_e_pending_notice "Creating basic config ${DEVICE_CONFIG_NAME}" "\'$(head -2  ${DINNER_CONF_DIR}/example.dist | sed 's/^###//g')\'"
+			_e_pending_notice "Creating basic config ${DEVICE_CONFIG_NAME}"
 		else
-			_e_notice "Creating basic config ${DEVICE_CONFIG_NAME}" "\"$(head -2  ${DINNER_CONF_DIR}/example.dist | sed 's/^###//g')\""
+			_e_notice "Creating basic config ${DEVICE_CONFIG_NAME}"
 		fi
+		printf "${BLDWHT}%s${TXTDEF}\n" "$(head -2  ${DINNER_CONF_DIR}/example.dist | sed 's/^###//g')"
 		printf "${DINNER_CONFIG_HEADER}\n" > ${DINNER_CONF_DIR}/${DEVICE_CONFIG_NAME}
 		old_IFS=$IFS
 		IFS=$'\n'
