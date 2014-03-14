bd462fcdc04a01fc6375592c11e50d60cf010c2b
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-14 22:17:19 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- dinner.sh
+++ dinner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/helper/dinner_completion.sh
+source ${HOME}/.dinner/helper/dinner_completion.sh
 
 function dinner () {
-	source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/helper/dinner_completion.sh
-	$(cd "$(dirname "${0}")" && pwd -P)/bin/dinner.sh "$@"
+	${HOME}/.dinner/bin/dinner.sh "$@"
 }
