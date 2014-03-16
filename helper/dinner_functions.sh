e2ea5c67b7610755108052561429424c7be14f7c
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2014-03-16 04:52:49 +0100

    Signed-off-by: Steven "NemesisRE" Koeberich <nemesissre@gmail.com>

--- helper/dinner_functions.sh
+++ helper/dinner_functions.sh
@@ -338,10 +338,8 @@ function _repo_pick () {
 	if [ "${#CURRENT_REPOPICK[@]}" ]; then
 		if [ -x ${REPO_DIR}/build/tools/repopick.py ]; then
 			export ANDROID_BUILD_TOP=${REPO_DIR}
-			for CHANGE in ${CURRENT_REPOPICK[@]}; do
-				_e_pending "Picking Gerrit ID ${CHANGE}..."
-				_exec_command "${REPO_DIR}/build/tools/repopick.py ${CHANGE}" "_e_pending_error \"Something went wrong while picking change ${CHANGE}\"" "_e_pending_success \"Successfully picked change ${CHANGE}\""
-			done
+			_e_pending "Picking Gerrit ID ${CHANGE}..."
+			_exec_command "${REPO_DIR}/build/tools/repopick.py ${REPOPICK_PARAMS} $(echo ${CURRENT_REPOPICK[@]})" "_e_pending_error \"Something went wrong while picking change ${CHANGE}\"" "_e_pending_success \"Successfully picked change ${CHANGE}\""
 		else
 			_e_warn "Could not find repopick.py, cannot make a repopick."
 		fi
